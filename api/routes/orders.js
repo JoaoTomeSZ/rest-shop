908c5c14f370de21dd2567bd5e878310d64bbe61
João Carlos <joaocarlos8221@gmail.com>
2025-05-30 21:07:08 -0300

    feat: Creating orders route

---
+++ api/routes/orders.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+        res.status(200).json({
+                message: 'Orders were fetched!'
+        })
+})
+
+router.post('/', (req, res, next) => {
+        res.status(201).json({
+                message: 'Orders was Created!'
+        })
+})
+
+router.get('/:orderId', (req, res, next) => {
+        res.status(200).json({
+                message: 'Orders was Created!',
+                orderId: req.params.orderId
+        })
+})
+
+
+module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router;
+const router = express.Router()
 
 router.get('/', (req, res, next) => {
         res.status(200).json({
@@ -11,6 +11,36 @@ router.post('/', (req, res, next) => {
         res.status(200).json({
                 message: 'Handling POST requests to /products'
         })
+})
+
+router.get('/:productId', (req, res, next) => {
+        const id = req.params.productId;
+        if(id === 'tao'){
+                res.status(200).json({
+                        message: 'Correct Id',
+                        id: id
+                })
+                
+        }
+        else{
+                res.status(200).json({
+                        message: 'You passed this Id: ' + id
+                })
+        }
+})
+
+router.patch('/:productId', (req, res, next) => {
+        const id = req.params.productId;
+                res.status(200).json({
+                        message: 'Updated product!'
+                })
+})
+
+router.delete('/:productId', (req, res, next) => {
+        const id = req.params.productId;
+                res.status(200).json({
+                        message: 'Deleted product!'
+                })
 })
 
 
